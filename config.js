0c55e128d8748aebb60b58f6f2789f37c7e93f1d
lyntretrau <90432151+lyntretrau@users.noreply.github.com>
2021-10-08 09:10:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,16 +4,16 @@ const CONFIG = {
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    btnIntro: '🥰🥰🥰',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Yêu anh nhìu <33',
+    btnYes: 'Yêu anh nhìu :33',
     btnNo: 'Không, Yêu anh hàng xóm :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh :3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Thích thì yêu thôi :33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Cho hun cái nè.',
-    btnAccept: 'Okiiiii lun <3',
+    btnAccept: 'Okiiiii lun :3',
     messLink: 'http:/www.facebook.com/baonao.thanh/' //
 }
